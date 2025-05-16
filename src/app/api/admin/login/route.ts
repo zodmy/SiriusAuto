de980afe9a2e255ddbd54fcbc0fce58ca4599da0
zodmy <zodmy 67293930+zodmy@users.noreply.github.com>
2025-05-16 18:36:32 +0300

    implement login page with bcrypt and middleware

---
+++ src/app/api/admin/login/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { serialize } from 'cookie';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export async function POST(req: NextRequest) {
+  try {
+    const { password } = await req.json();
+    console.log('HASH:', process.env.ADMIN_PASSWORD_HASH);
+    console.log('LENGTH:', process.env.ADMIN_PASSWORD_HASH?.length);
+
+    const storedHashedPassword = process.env.ADMIN_PASSWORD_HASH;
+
+    if (!storedHashedPassword) {
+      return NextResponse.json({ success: false, message: 'Серверна помилка: відсутній хеш пароля' }, { status: 500 });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, storedHashedPassword);
+
+    if (passwordMatch) {
+      const token = jwt.sign({ role: 'admin' }, process.env.JWT_SECRET!, {
+        expiresIn: '1d',
+      });
+
+      const cookie = serialize('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        path: '/',
+        maxAge: 60 * 60 * 24, // 1 день
+      });
+
+      return new NextResponse(JSON.stringify({ success: true }), {
+        status: 200,
+        headers: { 'Set-Cookie': cookie, 'Content-Type': 'application/json' },
+      });
+    } else {
+      return NextResponse.json({ success: false, message: 'Невірний пароль' }, { status: 401 });
+    }
+  } catch (error) {
+    console.error('Помилка обробки запиту:', error);
+    return NextResponse.json({ success: false, message: 'Серверна помилка: помилка обробки' }, { status: 500 });
+  }
+}
